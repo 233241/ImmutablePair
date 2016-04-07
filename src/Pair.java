2993878c0b1604f55f2b90b31c704b614df7bb17
m <233241ii@gmail.com>
2016-04-07 17:57:25 +0200

    class pair created

---
+++ src/Pair.java
@@ -0,0 +1,12 @@
+/**
+ * Created by m on 07.04.16.
+ */
+public class Pair<T1, T2> {
+    public final T1 x;
+    public final T2 y;
+
+    public Pair(T1 x, T2 y) {
+        this.x = x;
+        this.y = y;
+    }
+}
