0e117cc8a061cca5930fec22d14aa3bdf729444d
m <233241ii@gmail.com>
2016-04-07 18:44:53 +0200

    rename class

--- src/pair/ImmutablePair.java
+++ src/pair/ImmutablePair.java
@@ -3,11 +3,11 @@ package pair;
 /**
  * Created by m on 07.04.16.
  */
-public final class Pair<T1, T2> {
+public final class ImmutablePair<T1, T2> {
     public final T1 x;
     public final T2 y;
 
-    public Pair(T1 x, T2 y) {
+    public ImmutablePair(T1 x, T2 y) {
         this.x = x;
         this.y = y;
     }
